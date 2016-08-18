e6d337378eb8d7c57d9610938982f42fef4bf81d
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-08-18 14:37:58 -0400

    Finish assignment

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-$( document ).ready(function() {
+$( document ).ready(function() { // Only loads when the DOM is ready
 
   function appendItem(i){
-    var newLI = document.createElement('li');
-    newLI.innerHTML = i;
-    var list = $('ul');
-    list.appendChild(newLI);
-    return newLI
+    var liString = "<li>" + i + "</li>"
+    $('ul').append(liString);  // Appends the literal HTML `liString` to `ul`
   }
 
+  // JQuery seems to lack a convenient strikethrough method,
+  // so I've left this code alone. It's not particuarly verbose anyway.
   function removeItem(i){
     i.innerHTML = i.innerHTML.strike();
     setTimeout(function(){ i.remove(); }, 1000);
   }
 
-  $('#myButton').on("click", function(e){
-    var newI = $("#myField").value;
+  $('#myButton').on("click", function(e){ // Set EventListener on myButton
+    var newI = $("#myField").val();   // Get value of input box `#myField`
     appendItem(newI);
   });
 
-  $('ul').on('click', function(e){
+  $('ul').on('click', function(e){ // Set EventListener on the whole list
     if(e.target.tagName === "LI"){
       removeItem(e.target);
     }
